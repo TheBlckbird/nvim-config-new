1f6898cd0ddbd85414d8e37010ae889dec6bdf37
TheBlckbird <theblckbrd@proton.me>
2025-08-11 12:51:17 +0200

    Add commentary plugin

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -3,4 +3,22 @@ return {
     "cappyzawa/trim.nvim",
     opts = {},
   },
+
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      toggler = {
+        line = "gvv",
+        block = "gbv",
+      },
+      opleader = {
+        line = "gv",
+      },
+      extra = {
+        above = "gvO",
+        below = "gvo",
+        eol = "gvA",
+      },
+    },
+  },
 }
