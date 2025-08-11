655d847df4c9cfcd5f1c06c6b64d23b2bc1ea0cb
TheBlckbird <theblckbrd@proton.me>
2025-08-11 11:35:30 +0200

    Add editing tools

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -2,9 +2,9 @@ local set = vim.opt
 
 set.langmap = "xq,vw,le,cr,wt,kz,hu,gi,fo,qp,ßü,ua,is,ad,ef,og,sh,nj,rk,tl,dö,yä,üy,öx,äc,pv,zb,bn,mm,j-,XQ,VW,LE,CR,WT,KZ,HU,GI,FO,QP,ẞÜ,UA,IS,AD,EF,OG,SH,NJ,RK,TL,DÖ,YÄ,ÜY,ÖX,ÄC,PV,ZB,BN,MM,J-,§$,$§"
 
-set.tabstop = 2
+set.tabstop = 4
 set.expandtab = true
-set.shiftwidth = 2
+set.shiftwidth = 4
 set.smarttab = true
 set.nu = true
 set.rnu = true
@@ -14,3 +14,4 @@ set.cursorline = true
 
 vim.g.mapleader = " "
 -- vim.g.maplocalleader = "\\"
+
