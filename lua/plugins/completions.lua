b9770ea94f3d9485234e0baee341fec5a6870e5c
TheBlckbird <theblckbrd@proton.me>
2025-08-11 13:53:04 +0200

    Add icons and colors to completions menu

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,6 +12,7 @@ return {
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
       local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
 
       cmp.setup({
         snippet = {
@@ -60,9 +61,42 @@ return {
           { name = "buffer" },
           { name = "lazydev", group_index = 0 },
         }),
+
+        formatting = {
+          format = lspkind.cmp_format(),
+        },
       })
 
+      -- Add brackets after functions
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on(
+        "confirm_done",
+        cmp_autopairs.on_confirm_done()
+      )
+
+      -- Visual Studio Code Dark+ colors for ontries
+
+      -- gray
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#808080' })
+      -- blue
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
+      vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpIntemAbbrMatch' })
+      -- light blue
+      vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
+      -- pink
+      vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
+      -- front
+      vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
+      vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
     end
+  },
+
+  {
+    "onsails/lspkind.nvim",
   },
 
   -- Sources
